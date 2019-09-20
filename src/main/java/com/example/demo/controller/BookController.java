9c77d6615a8e5ee5e811c49437fa253ca4f2a722
xin <chenxin0049@gmail.com>
2019-09-20 17:45:23 +0800

    Book控制器

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Book;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 public class BookController {
     @GetMapping("/books")
     public ModelAndView book() {
