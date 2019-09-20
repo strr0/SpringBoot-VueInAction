9153a1bbb02868b820a015bca968f9ea25d97b03
xin <chenxin0049@gmail.com>
2019-09-20 17:00:10 +0800

    Book控制器

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -3,12 +3,21 @@ package com.example.demo.controller;
 import com.example.demo.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
-    @GetMapping("/book")
-    public Book book() {
-        Book book = new Book(1, "三国演义", "罗贯中");
-        return book;
+    @GetMapping("/books")
+    public ModelAndView book() {
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book(1, "三国演义", "罗贯中"));
+        bookList.add(new Book(2, "红楼梦", "曹雪芹"));
+        ModelAndView mv = new ModelAndView();
+        mv.addObject(bookList);
+        mv.setViewName("books");
+        return mv;
     }
 }
