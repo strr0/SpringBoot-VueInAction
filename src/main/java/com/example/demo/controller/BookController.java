070a3c715c13781be12280bfb9dd34bb57828376
xin <chenxin0049@gmail.com>
2019-09-26 22:04:17 +0800

    Book控制器

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,16 +1,25 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Book;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
 @RestController
+@RequestMapping("/book")
 public class BookController {
-    @GetMapping("/book")
+    @GetMapping("/")
     public Book book() {
         Book book = new Book("三国演义", "罗贯中", 30f, new Date());
         return book;
+    }
+
+    @PostMapping("/")
+    public String addBook(String name) {
+        return "receive:" + name;
+    }
+    @DeleteMapping("/{id}")
+    public String deleteBookById(@PathVariable Long id) {
+        return String.valueOf(id);
     }
 }
