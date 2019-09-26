0cc96ea6c01edcebafd48985c5a1c5cad8b351b4
xin <chenxin0049@gmail.com>
2019-09-26 22:04:56 +0800

    Hello控制器

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello spring boot!";
+    public void hello(Model model) {
+        Map<String, Object> map = model.asMap();
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ">>>>>" + entry.getValue());
+        }
     }
 }
