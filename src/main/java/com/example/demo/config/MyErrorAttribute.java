296bd21a0a2f065401713cc81b954e80da718d4f
xin <chenxin0049@gmail.com>
2019-09-26 22:00:32 +0800

    自定义Error数据

---
+++ src/main/java/com/example/demo/config/MyErrorAttribute.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Component
+public class MyErrorAttribute extends DefaultErrorAttributes {
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+        errorAttributes.put("custommsg", "出错啦!");
+        return errorAttributes;
+    }
+}
