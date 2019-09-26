3f0b6bbd540b802607a722540abdeaa2f418d230
xin <chenxin0049@gmail.com>
2019-09-26 22:06:38 +0800

    SpringBoot测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,21 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.servlet.ServletContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+    @Autowired
+    private ServletContext servletContext;
 
     @Test
     public void contextLoads() {
+        System.out.println(servletContext.getRealPath("/"));
     }
 
 }
