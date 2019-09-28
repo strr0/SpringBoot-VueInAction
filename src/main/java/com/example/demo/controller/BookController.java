c769095e5bc3030bf2a1a0cac8517ce543926e54
xin <chenxin0049@gmail.com>
2019-09-28 22:54:43 +0800

    Book控制器

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,23 +1,23 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
     @GetMapping("/books")
-    public ModelAndView books() {
-        List<Book> bookList = new ArrayList<>();
-        bookList.add(new Book(1, "三国演义", "罗贯中"));
-        bookList.add(new Book(2, "红楼梦", "曹雪芹"));
-        ModelAndView mv = new ModelAndView();
-        mv.addObject(bookList);
-        mv.setViewName("books");
-        return mv;
+    public String book(Model model) {
+        List<Book> bookList = bookService.getBooks();
+        model.addAttribute(bookList);
+        return "books";
     }
 }
