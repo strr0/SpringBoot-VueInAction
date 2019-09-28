99f64fa4dcd55ff11b54b44dbc8993e326e5c2b1
xin <chenxin0049@gmail.com>
2019-09-28 15:35:06 +0800

    过滤器

---
+++ src/main/java/com/example/demo/config/MyFilter.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("MyFilter>>>init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("MyFilter>>>doFilter");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("MyFilter>>>destroy");
+    }
+}
