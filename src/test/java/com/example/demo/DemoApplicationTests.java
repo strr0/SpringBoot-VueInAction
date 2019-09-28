a094fd7a4dfa8ba3bfb5cac41c992011c17cac2d
xin <chenxin0049@gmail.com>
2019-09-28 21:59:10 +0800

    mybatis测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.entity.Book;
+import com.example.demo.mapper.BookMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+    @Resource
+    private BookMapper bookMapper;
 
     @Test
     public void contextLoads() {
+        Book book = bookMapper.getBookById(1);
+        Assert.assertNotNull(book);
+        System.out.println(book.getName());
     }
 
 }
