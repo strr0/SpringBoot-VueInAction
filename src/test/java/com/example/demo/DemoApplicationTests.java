aa28034294c2f67a96ffe38be66afdce8507afbc
xin <chenxin0049@gmail.com>
2019-09-28 20:44:31 +0800

    jdbc测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,41 @@
 package com.example.demo;
 
+import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+    @Autowired
+    BookService bookService;
 
     @Test
     public void contextLoads() {
+        List<Book> bookList = bookService.getAllBooks();
+        Assert.assertNotNull(bookList);
+        for (Book book : bookList) {
+            System.out.println(book.getName());
+        }
+    }
+
+    @Test
+    public void testAddBook() {
+        int result = bookService.addBook(new Book(null, "西厢记", "王实甫"));
+        System.out.println(result);
+    }
+
+    @Test
+    public void testDeleteBook() {
+        int result = bookService.deleteBook(3);
+        System.out.println(result);
     }
 
 }
