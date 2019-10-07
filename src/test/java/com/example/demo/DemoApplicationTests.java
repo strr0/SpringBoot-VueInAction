53a9ae5890e7b075a4593566747661b347906d0f
xin <chenxin0049@gmail.com>
2019-10-07 22:26:57 +0800

    MockMvc测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,27 +1,31 @@
 package com.example.demo;
 
-import com.example.demo.entity.Book;
-import com.example.demo.repository.BookRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
     @Autowired
-    private BookRepository bookRepository;
+    private WebApplicationContext context;
 
     @Test
-    public void contextLoads() {
-        List<Book> bookList = bookRepository.findAll();
-        Assert.assertNotNull(bookList);
-        System.out.println(bookList.get(0).getName());
+    public void contextLoads() { }
+
+    @Test
+    public void testController() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/books"))
+                .andExpect(MockMvcResultMatchers.view().name("books"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("bookList"));
     }
 
 }
