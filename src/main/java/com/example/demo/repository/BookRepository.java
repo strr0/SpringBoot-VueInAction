eeddb61ddff100ecf3770e0add54672d042929ee
xin <chenxin0049@gmail.com>
2019-10-07 15:56:24 +0800

    实现Rest

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -2,11 +2,16 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "bs", collectionResourceRel = "bs", itemResourceRel = "b")
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    void deleteById(Integer id);
-    Book findOneById(Integer id);
-    List<Book> findAll();
+    @RestResource(path = "author", rel = "author")
+    List<Book> findByAuthorContains(@Param("author") String author);
+    @RestResource(path = "name", rel = "name")
+    Book findByName(@Param("name") String name);
 }
