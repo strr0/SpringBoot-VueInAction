23b44c96b53964858c8acad3114cf466f81f1226
xin <chenxin0049@gmail.com>
2019-10-10 16:53:19 +0800

    缓存测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,27 +1,40 @@
 package com.example.demo;
 
+import com.example.demo.dao.BookDao;
 import com.example.demo.entity.Book;
-import com.example.demo.repository.BookRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
     @Autowired
-    private BookRepository bookRepository;
+    private BookDao bookDao;
 
     @Test
     public void contextLoads() {
-        List<Book> bookList = bookRepository.findAll();
-        Assert.assertNotNull(bookList);
-        System.out.println(bookList.get(0).getName());
+        System.out.print("第一次get: ");
+        bookDao.getBookById(1);
+        System.out.println();
+
+        System.out.print("第二次get: ");
+        bookDao.getBookById(1);
+        System.out.println();
+
+        System.out.print("delete: ");
+        bookDao.deleteBookById(1);
+        System.out.println();
+
+        System.out.print("update: ");
+        bookDao.updateBookById(new Book(1, "三国演义", "罗贯中"));
+        System.out.println();
+
+        System.out.print("第三次get: ");
+        bookDao.getBookById(1);
+        System.out.println();
     }
 
 }
