e9fd93a7667ea0a868cc6fff94defc2c3d549387
xin <chenxin0049@gmail.com>
2019-10-10 16:51:29 +0800

    Book实体

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -1,14 +1,8 @@
 package com.example.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.io.Serializable;
 
-@Entity
-public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Book implements Serializable {
     private Integer id;
     private String name;
     private String author;
