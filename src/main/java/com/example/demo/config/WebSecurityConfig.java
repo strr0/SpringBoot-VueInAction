8db14fc5851ac39d57ab68887dce322023834c5f
xin <chenxin0049@gmail.com>
2019-11-05 17:35:21 +0800

    Oauth2配置

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,9 +1,46 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String password = new BCryptPasswordEncoder().encode("123456");
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(password)
+                .roles("admin")
+                .and()
+                .withUser("strr")
+                .password(password)
+                .roles("user");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/oauth/**").authorizeRequests()
+                .antMatchers("/oauth/**").permitAll()
+                .and()
+                .csrf().disable();
+    }
 }
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,27 +1,22 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.MethodService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @Autowired
-    private MethodService service;
-
     @GetMapping("/admin/hello")
     public String admin() {
-        return service.admin();
+        return "Hello admin";
     }
 
     @GetMapping("/user/hello")
     public String user() {
-        return service.user();
+        return "Hello user";
     }
 
-    @GetMapping("/dba/hello")
+    @GetMapping("/hello")
     public String dba() {
-        return service.dba();
+        return "Hello";
     }
 }
