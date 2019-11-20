7063e9e77b1bda747303204542d6d5ce109fdd32
xin <chenxin0049@gmail.com>
2019-11-20 22:15:49 +0800

    JavaMail的使用

--- src/main/java/com/example/demo/config/MailService.java
+++ src/main/java/com/example/demo/config/MailService.java
@@ -1,9 +1,15 @@
 package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Component
 public class MailService {
@@ -17,5 +23,46 @@ public class MailService {
         simpleMailMessage.setSubject(subject);
         simpleMailMessage.setText(content);
         javaMailSender.send(simpleMailMessage);
+    }
+
+    public void sendAttachFileMail(String from, String to, String subject, String content, File file) {
+        try{
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content);
+            helper.addAttachment(file.getName(), file);
+            javaMailSender.send(mimeMessage);
+        }
+        catch (MessagingException m) {
+            m.printStackTrace();
+        }
+    }
+
+    public void sendMailWithImg(String from, String to, String subject, String content,
+                                String[] srcPath, String[] resIds) {
+        if(srcPath.length != resIds.length) {
+            System.out.println("发送失败");
+            return;
+        }
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            for(int i = 0; i < srcPath.length; i++) {
+                FileSystemResource res = new FileSystemResource(new File(srcPath[i]));
+                helper.addInline(resIds[i], res);
+            }
+            javaMailSender.send(mimeMessage);
+        }
+        catch (MessagingException m) {
+            System.out.println("发送失败");
+            m.printStackTrace();
+        }
     }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
@@ -19,6 +21,29 @@ public class DemoApplicationTests {
                 "",
                 "测试邮件主题",
                 "测试邮件内容");
+    }
+
+    @Test
+    public void sendAttachFileMail() {
+        mailService.sendAttachFileMail("",
+                "",
+                "测试邮件主题",
+                "测试邮件内容",
+                new File(""));
+    }
+
+    @Test
+    public void sendMailWithImg() {
+        mailService.sendMailWithImg("",
+                "",
+                "测试邮件主题（图片）",
+                "<div>hello, 这是一封带图片资源的邮件: " +
+                        "这是图片1: <div><img src='cid:p01'/></div>" +
+                        "这是图片2: <div><img src='cid:p02'/></div>" +
+                        "</div>",
+                new String[]{"",
+                        ""},
+                new String[]{"p01", "p02"});
     }
 
 }
