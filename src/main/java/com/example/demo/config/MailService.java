80e483ff22bf75685526ddfa6bb5e4830aecd3c2
xin <chenxin0049@gmail.com>
2019-11-20 16:09:42 +0800

    JavaMail的使用

---
+++ src/main/java/com/example/demo/config/MailService.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendSimpleMail(String from, String to, String subject, String content) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(from);
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(content);
+        javaMailSender.send(simpleMailMessage);
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.config.MailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+    @Autowired
+    private MailService mailService;
 
     @Test
-    public void contextLoads() {
+    public void sendSimpleMail() {
+        mailService.sendSimpleMail("",
+                "",
+                "测试邮件主题",
+                "测试邮件内容");
     }
 
 }
