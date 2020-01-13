c489be0eef2c8c13c8ed6628e787b8080bf8008b
xin <chenxin0049@gmail.com>
2020-01-13 14:32:42 +0800

    授权管理

---
+++ src/main/java/com/example/demo/config/CustomDecisionManager.java
@@ -0,0 +1,47 @@
+package com.example.demo.config;
+
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class CustomDecisionManager implements AccessDecisionManager {
+    @Override
+    public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for(ConfigAttribute configAttribute : configAttributes) {
+            if("ROLE_LOGIN".equals(configAttribute.getAttribute())) {
+                if(authentication instanceof AnonymousAuthenticationToken) {
+                    throw new BadCredentialsException("未登录");
+                }
+                else{
+                    return;
+                }
+            }
+            for(GrantedAuthority authority : authorities) {
+                if(authority.getAuthority().equals(configAttribute.getAttribute())) {
+                    return;
+                }
+            }
+        }
+        throw new AccessDeniedException("权限不足");
+    }
+
+    @Override
+    public boolean supports(ConfigAttribute attribute) {
+        return true;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return true;
+    }
+}
