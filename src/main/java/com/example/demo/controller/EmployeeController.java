893b0f57820872dd4630efae421e6d3e4a0a14a1
xin <chenxin0049@gmail.com>
2020-02-04 21:15:02 +0800

    控制器

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,11 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Employee;
+import com.example.demo.entity.*;
 import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -13,11 +11,12 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/employee/basic")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/employee/basic")
+    @GetMapping("/")
     public Map<String, Object> getEmployeeByPage(@RequestParam(defaultValue = "1") Integer page,
                                                  @RequestParam(defaultValue = "10") Integer size,
                                                  Employee employee, Date[] beginDateScope) {
@@ -27,5 +26,48 @@ public class EmployeeController {
         map.put("emps", employeeByPage);
         map.put("total", total);
         return map;
+    }
+
+    @GetMapping("/maxWorkId")
+    public Integer getMaxWorkId() {
+        return employeeService.getMaxWorkId();
+    }
+
+    @PostMapping("/")
+    public Integer addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+    @PutMapping("/")
+    public Integer editEmployee(@RequestBody Employee employee) {
+        return employeeService.editEmployee(employee);
+    }
+    @DeleteMapping("/{id}")
+    public Integer deleteEmployee(@PathVariable Integer id) {
+        return employeeService.deleteEmployee(id);
+    }
+
+    @GetMapping("/politics")
+    public List<Politic> getAllPolitics() {
+        return employeeService.getAllPolitics();
+    }
+
+    @GetMapping("/nations")
+    public List<Nation> getAllNations() {
+        return employeeService.getAllNations();
+    }
+
+    @GetMapping("/positions")
+    public List<Position> getAllPositions() {
+        return employeeService.getAllPositions();
+    }
+
+    @GetMapping("/jobLevels")
+    public List<JobLevel> getAllJobLevels() {
+        return employeeService.getAllJobLevels();
+    }
+
+    @GetMapping("/departments")
+    public List<Department> getAllDepartments() {
+        return employeeService.getAllDepartments();
     }
 }
